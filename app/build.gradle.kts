ed8489644996f3b0c17540ee52b95f8cdc6d8a7b
Gothwad Tech <gothwadtech@gmail.com>
2026-09-19 13:23:22 +0530

    build: disable lint checks and simplify launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,10 @@ android {
     compose = true
     buildConfig = true
   }
+  lint {
+    abortOnError = false
+    checkReleaseBuilds = false
+  }
   testOptions { unitTests { isIncludeAndroidResources = true } }
 }
 
